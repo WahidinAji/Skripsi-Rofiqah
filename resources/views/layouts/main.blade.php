4331a9e7aae92e187cbd2669539635b7a4e24291
WahidinAji <a17wahidin@gmail.com>
2021-01-15 09:58:23 +0700

    delete form search on navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,9 +19,9 @@
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
                 <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+                    <input hidden class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
                     <div class="input-group-append">
-                        <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                        <!-- <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button> -->
                     </div>
                 </div>
             </form>

4e7000e89b8c707c48ab22364127c2628e2542a2
WahidinAji <a17wahidin@gmail.com>
2021-09-04 00:03:34 +0700

    update error in main file

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -71,6 +71,24 @@
             </nav>
         </div>
         <div id="layoutSidenav_content">
+            <div class="container-fluid">
+                @if($errors->any())
+                <div class="row align-items-start ml-0 mt-3">
+                    @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert" aria-live="polite" aria-atomic="true"  data-delay="50000">
+                        {{ $error }}
+                        <div type="button" class="close" data-dismiss="alert">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-backspace-fill"
+                                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M15.683 3a2 2 0 0 0-2-2h-7.08a2 2 0 0 0-1.519.698L.241 7.35a1 1 0 0 0 0 1.302l4.843 5.65A2 2 0 0 0 6.603 15h7.08a2 2 0 0 0 2-2V3zM5.829 5.854a.5.5 0 1 1 .707-.708l2.147 2.147 2.146-2.147a.5.5 0 1 1 .707.708L9.39 8l2.146 2.146a.5.5 0 0 1-.707.708L8.683 8.707l-2.147 2.147a.5.5 0 0 1-.707-.708L7.976 8 5.829 5.854z" />
+                            </svg>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+                @endif
+            </div>
             @yield('main')
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid">
